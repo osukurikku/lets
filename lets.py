3f9cd76a89045168597920cadbd7bf6dfad1d7b8
Mike Shenin <i@kotrik.ru>
2018-12-08 14:34:15 +0300

    Update requests

--- lets.py
+++ lets.py
@@ -64,8 +64,10 @@ def make_app():
 		(r"/s/(.*)", downloadMapHandler.handler),
 		(r"/web/replays/(.*)", getFullReplayHandler.handler),
 
-		(r"/p/verify", redirectHandler.handler, dict(destination="https://ripple.moe/index.php?p=2")),
-		(r"/u/(.*)", redirectHandler.handler, dict(destination="https://ripple.moe/index.php?u={}")),
+		(r"/p/verify", redirectHandler.handler, dict(destination="https://kotorikku.ru/")),
+		(r"/u/(.*)", redirectHandler.handler, dict(destination="https://kotorikku.ru/u/{}")),
+		(r"/b/(.*)", redirectHandler.handler, dict(destination="https://kotorikku.ru/b/{}")),
+		(r"/", redirectHandler.handler, dict(destination="https://kotorikku.ru/"))
 
 		(r"/api/v1/status", apiStatusHandler.handler),
 		(r"/api/v1/pp", apiPPHandler.handler),
