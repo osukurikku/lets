b442c8e616c244364d82ea84fa176e5d58242f93
Mike Shenin <i@kotrik.ru>
2019-03-20 22:32:04 +0300

    This should works.

--- objects/score.pyx
+++ objects/score.pyx
@@ -287,7 +287,7 @@ class score:
 
 			# No duplicates found.
 			# Get right "completed" value
-			personalBest = glob.db.fetch("SELECT id, score FROM scores WHERE userid = %s AND beatmap_md5 = %s AND play_mode = %s AND completed = 3 LIMIT 1", [userID, self.fileMd5, self.gameMode])
+			personalBest = glob.db.fetch("SELECT id, score, pp FROM scores WHERE userid = %s AND beatmap_md5 = %s AND play_mode = %s AND completed = 3 LIMIT 1", [userID, self.fileMd5, self.gameMode])
 			if personalBest is None:
 				# This is our first score on this map, so it's our best score
 				self.completed = 3
@@ -297,7 +297,7 @@ class score:
 			else:
 				self.personalOldBestScore = personalBest["id"]
 				# Compare personal best's score with current score
-				if self.score > personalBest["score"]:
+				if self.score > personalBest["score"] or self.pp > personalBest["pp"]:
 					# New best score
 					self.completed = 3
 					self.rankedScoreIncrease = self.score-personalBest["score"]
