47200c0bbc0e7e15b742e9d260638c1586d00a98
Mike Shenin <i@kotrik.ru>
2019-12-20 20:10:24 +0300

    upd: dont cache values if relax

--- handlers/apiPPHandler.py
+++ handlers/apiPPHandler.py
@@ -109,6 +109,9 @@ class handler(requestsManager.asyncRequestHandler):
 					# Generic acc
 					# Get cached pp values
 					cachedPP = bmap.getCachedTillerinoPP()
+					if modsEnum&mods.RELAX or modsEnum&mods.RELAX2:
+						cachedPP = [0,0,0,0]
+
 					if cachedPP != [0,0,0,0]:
 						log.debug("Got cached pp.")
 						returnPP = cachedPP
@@ -123,10 +126,11 @@ class handler(requestsManager.asyncRequestHandler):
 						returnPP = oppai.pp
 						bmap.starsStd = oppai.stars
 
-						# Cache values in DB
-						log.debug("Saving cached pp...")
-						if type(returnPP) == list and len(returnPP) == 4:
-							bmap.saveCachedTillerinoPP(returnPP)
+						if not modsEnum&mods.RELAX or modsEnum&mods.RELAX2:
+							# Cache values in DB
+							log.debug("Saving cached pp...")
+							if type(returnPP) == list and len(returnPP) == 4:
+								bmap.saveCachedTillerinoPP(returnPP)
 				else:
 					# Specific accuracy, calculate
 					# Create oppai instance
