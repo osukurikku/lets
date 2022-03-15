28dd32bc68e604e6dc7dcd791a5861bdbc3808a0
Mike Shenin <i@kotrik.ru>
2022-03-15 12:44:43 +0300

    feat: another rust calculator

--- pp/__init__.py
+++ pp/__init__.py
@@ -1,9 +1,10 @@
 from common.constants import gameModes
 
-from pp import ez, ez_peace
+from pp import ez, ez_peace, ez_rosu_pp
 
 PP_CALCULATORS = {
     gameModes.STD: ez_peace.EzPeace,
+    # gameModes.STD: ez_rosu_pp.EzRosu,
     gameModes.TAIKO: ez_peace.EzPeace,
     gameModes.CTB: ez_peace.EzPeace,
     gameModes.MANIA: ez_peace.EzPeace
